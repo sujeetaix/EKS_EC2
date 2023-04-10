6e268e0fb5279e7ac65c5b56d3aeccb4c1d876b9
praneeth wickramaarachchi <wpraneethmadusanka@gmail.com>
2023-04-10 21:50:18 +0530

    issue fixed

--- subnets.tf
+++ subnets.tf
@@ -44,7 +44,7 @@ resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidr_blocks)
 
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
 
 # *********************
